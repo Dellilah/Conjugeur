c8a8c84d0743d9e64c48c38538c3d97a7f705f36
Alicja <acyganiewicz@gmail.com>
2016-06-29 00:27:07 +0200

    CC refactor: AH where in/not in

--- app/models/active_hash_where.rb
+++ app/models/active_hash_where.rb
@@ -1,16 +1,18 @@
 module ActiveHashWhere
   def where_in(options)
-    result = all
-    options.each do |key, values|
-      result = result & (all.select{ |v| values.include?(v.send(key))})
-    end
-    result
+    retrieve_elements(:&, options)
   end
 
   def where_not_in(options)
+    retrieve_elements(:-, options)
+  end
+
+  private
+
+  def retrieve_elements(relation, options)
     result = all
     options.each do |key, values|
-      result = result - (all.select{ |v| values.include?(v.send(key))})
+      result = result.send(relation, all.select{ |v| values.include?(v.send(key))})
     end
     result
   end
